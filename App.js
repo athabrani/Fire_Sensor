f8f325d82edf385879ff410f96a5025e20e9bc5d
marialorenza <160384456+marialorenza@users.noreply.github.com>
2024-03-18 00:11:09 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Footer from './components/footer';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <div>
+        <profil /> 
+       </div>
+       <Footer />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
