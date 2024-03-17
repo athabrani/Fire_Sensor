80ab5f3b1257e40e052294880280f1dcfbd53c2e
Faturthir <144870506+Faturthir@users.noreply.github.com>
2024-03-17 23:48:57 +0700

    Add files via upload

---
+++ firebase/Firebaseconfig.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBCW32Oic-qcikjjEm9qElqoZfMkT3XqQk",
+  authDomain: "kelompok5-39a6c.firebaseapp.com",
+  databaseURL: "https://kelompok5-39a6c-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "kelompok5-39a6c",
+  storageBucket: "kelompok5-39a6c.appspot.com",
+  messagingSenderId: "916895058016",
+  appId: "1:916895058016:web:f4115dd5cf29d18bdd9b05",
+  measurementId: "G-L36149X96C"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export {db};
